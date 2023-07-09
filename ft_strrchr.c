8ab7e8b469300368c6a2a281702d5408423f3fc7
Muhammed Uğur EMRE <101524933+muguremre@users.noreply.github.com>
2023-07-09 18:23:36 +0300

    I wrote my own libft library with C programming.

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,23 +6,26 @@
 /*   By: memre <42istanbul.com.tr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/05 20:31:06 by memre             #+#    #+#             */
-/*   Updated: 2023/07/09 16:06:54 by memre            ###   ########.tr       */
+/*   Updated: 2023/07/09 17:57:36 by memre            ###   ########.tr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include "libft.h"
 
 char	*ft_strrchr(const char *s, int c)
 {
 	int		i;
+	char	*ptr;
 
-	i = ft_strlen(s);
-	if (c == 0)
-		return ((char *)s + i);
-	while (i >= 0)
+	i = 0;
+	ptr = 0;
+	while (s[i])
 	{
-		if (s[i] == c)
-			return ((char *)s + i);
-		i--;
+		if (s[i] == (char)c)
+			ptr = (char *)(s + i);
+		i++;
 	}
-	return (NULL);
+	if (s[i] == (char)c)
+		ptr = (char *)(s + i);
+	return (ptr);
 }
--- libft.h
+++ libft.h
@@ -71,6 +71,8 @@ char			*ft_strchr(const char *s, int c);
 
 char			*ft_strdup(const char *s1);
 
+void			ft_striteri(char *s, void (*f)(unsigned int, char *));
+
 char			*ft_strjoin(char const *s1, char const *s2);
 
 size_t			ft_strlcat(char *dst, const char *src, size_t size);
